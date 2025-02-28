c43222a44bc583efae25db5c09ebff2e01491ec5
Laila Khan <lailatariqkhan@gmail.com>
2025-02-28 11:58:09 +0000

    Part Four in progress

--- FileReader.cs
+++ FileReader.cs
@@ -1,40 +1,59 @@
 
-using System.Transactions;
+using NLog;
 
 namespace SupportBank
 {
     class FileReader
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
         public static void ReadFile(string filepath, List<Transaction> transactions)
         {
             try
             {
                 if (!File.Exists(filepath))
+                {
+                    Logger.Error($"File Not Found: {filepath}");
                     throw new FileLoadException("Error: Transaction file not found");
-
+                }
                 string[] lines = File.ReadAllLines(filepath);
+                int linenumber = 1;
+
                 foreach (string line in lines.Skip(1))
                 {
                     if (string.IsNullOrWhiteSpace(line)) continue;
-
                     string[] datapoint = line.Split(',');
-
-                    DateOnly date = DateOnly.Parse(datapoint[0]);
+                    if (datapoint.Length != 5)
+                    {
+                        Logger.Warn($"Invalid transaction format with " +
+                        $"inaccurate column length {datapoint.Length} in line {linenumber})");
+                    }
+                    if (!DateOnly.TryParse(datapoint[0], out DateOnly date))
+                    {
+                        Logger.Error($"Invalid date format: {datapoint[0]} in line {linenumber})");
+                        throw new FormatException($"Invalid date format: {datapoint[0]} in line {linenumber})");
+                    }
                     string from = datapoint[1];
                     string to = datapoint[2];
                     string narrative = datapoint[3];
-                    int amount = Convert.ToInt32(float.Parse(datapoint[4]));
-
+                    if (!float.TryParse(datapoint[4], out float parsedamount))
+                    {
+                        Logger.Error($"Invalid amount:{datapoint[4]} in line {linenumber})");
+                        throw new FormatException($"Invalid amount:{datapoint[4]} in line {linenumber})");
+                    }
+                    int amount = Convert.ToInt32(parsedamount);
                     transactions.Add(new Transaction(date, from, to, narrative, amount));
+                    linenumber++;
                 }
             }
             catch (FileNotFoundException error)
             {
-                Console.WriteLine($"Error: {error.Message}");
+                Logger.Error($"Error:{error}");
+                Console.WriteLine($"Error:{error}");
             }
             catch (IOException error)
             {
-                Console.WriteLine($"Error: {error.Message}");
+                Logger.Fatal($"Error Reading file: {error}");
+                Console.WriteLine($"Error:{error.Message}");
             }
         }
 
--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
 namespace SupportBank
 {
     class SupportBank
     {
         public static void Main()
         {
+            var config = new LoggingConfiguration();
+            var target = new FileTarget { FileName = @"C:\Users\LaiKha\Training\SupportBankApp\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+            LogManager.Configuration = config;
+            
             AccountHandler accounthandler = new AccountHandler();
             List<Transaction> transactions = new List<Transaction>();
 
-            string filepath = "Transactions2014.csv";
+            string filepath = "DodgyTransactions2015.csv";
             FileReader.ReadFile(filepath, transactions);
             accounthandler.LoadAccountTransactions(transactions);
 
